9a936ba11450c442c6e82ae7ebf1a90a2a2d8e54
Blen <62964622+blenassefa2@users.noreply.github.com>
2026-03-03 18:21:39 +0000

    Complete exercise 1: confirm MPU TYPE Register corresponds correctly with CMSIS_5 device header file

--- M33/main.c
+++ M33/main.c
@@ -1,6 +1,15 @@
+#include "ARMCM33_DSP_FP.h"
+
 int main(void) {
-    volatile int x = 42;
-    volatile int y = x + 1;
-    (void)y;
+    unsigned int num_regions = (MPU->TYPE >> 8) & 0xFF;
+
+    // After this line is executed on gdb print result to see result value 
+    // then compare it  with 
+    // print (*(unsigned int*)0xE000ED90 >> 8) & 0xFF
+    // 0xE000ED90 is the address of MPU_TYPE register
+    //      (Remember this register is memory type so you infer it through its memory address 
+    //      and you can't examine it with info registers)
+    volatile unsigned int result = num_regions;
+
     while (1);
 }
